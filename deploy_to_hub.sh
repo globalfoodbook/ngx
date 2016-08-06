f273b8deaa06119471d78e65b571ddd6102b7469
Ikenna Okpala <me@ikennaokpala.com>
2016-08-06 18:46:08 +0100

    Add docker hub deployment script

---
+++ deploy_to_hub.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Make sure to start boot2docker before running this script
+eval "$(docker-machine env default)"
+
+/usr/local/bin/docker login
+
+read -p "What version number or tag are you deploying? " tag
+
+if [[ $tag ]]; then
+  /usr/local/bin/docker build -t globalfoodbook/ngxl:$tag .
+  /usr/local/bin/docker push globalfoodbook/ngxl:$tag
+
+  /usr/local/bin/docker build -t globalfoodbook/ngxl:latest .
+  /usr/local/bin/docker push globalfoodbook/ngxl:latest
+else
+  echo -e "Kindly provide a tag or version number"
+fi
