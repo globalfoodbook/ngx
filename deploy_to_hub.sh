4f943f159533da8dbbc5ea899262bf1cd869c224
Ikenna Okpala <me@ikennaokpala.com>
2016-09-05 00:08:12 +0100

    Add docker build Args and Labels

--- deploy_to_hub.sh
+++ deploy_to_hub.sh
@@ -8,10 +8,10 @@ eval "$(docker-machine env default)"
 read -p "What version number or tag are you deploying? " tag
 
 if [[ $tag ]]; then
-  /usr/local/bin/docker build -t globalfoodbook/ngxl:$tag .
+  /usr/local/bin/docker build --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` --build-arg VCS_REF=`git rev-parse --short HEAD` -t globalfoodbook/ngxl:$tag .
   /usr/local/bin/docker push globalfoodbook/ngxl:$tag
 
-  /usr/local/bin/docker build -t globalfoodbook/ngxl:latest .
+  /usr/local/bin/docker build --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` --build-arg VCS_REF=`git rev-parse --short HEAD` -t globalfoodbook/ngxl:latest .
   /usr/local/bin/docker push globalfoodbook/ngxl:latest
 else
   echo -e "Kindly provide a tag or version number"
